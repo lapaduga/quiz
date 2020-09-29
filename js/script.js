b9ea5f3bd44c60d0a2f7db23f6f764bbb98dc29f
lapaduga <deniskotov29042015@gmail.com>
2020-09-29 20:57:49 +0500

    h

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-	$('.aside__hide').click(function(event){
-		$('.aside, .aside__hide, .aside__title, .aside__menu, .main, .profile').toggleClass('passive');
+	$('.aside__hide').click(function (event) {
+		$('.aside, .aside__hide, .aside__title, .aside__menu, .main, .profile, .profile__image, .profile__technical, .decor, .profile__name, .profile__position, .profile__tests-done, .profile__finished-tests').toggleClass('passive');
 	});
 });
